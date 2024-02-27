c05c7b39e437d9ec33766b5138ba4eb7066be6c1
Alessandro Gaggia <alex@besharp.it>
2024-02-27 23:32:56 +0100

    chore: new genie 2.0

---
+++ Utils/Craft.cs
@@ -0,0 +1,11 @@
+﻿namespace XPirateZ_Genie2._0.Utils
+{
+    internal class Craft
+    {
+        public string Type { get; set; }
+        public int FuelMax { get; set; }
+        public int DamageMax { get; set; }
+        public int ShieldCapacity { get; internal set; }
+        public int SpriteId { get; set; }
+    }
+}
